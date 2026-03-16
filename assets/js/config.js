82e82f559e9e2b6f4456511a2c0aa15ef42b68e5
Ksawery Szkudlarski <szkudlarski.ksawery@gmail.com>
2026-03-16 21:47:11 +0100

    Reformat Java entries in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -35,17 +35,17 @@ window.SH = {
       ],
     },
      poc: {
-        java: [{name: 'algorytm.java', path: 'algorytm.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'JavaCasting', path: 'JavaCasting.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'JavaConditions', path: 'JavaConditions.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'kalkulator', path: 'kalkulator.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'Liczba Pierwsza', path: 'LiczbaPierwsza.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'LiczbyParzyste', path: 'LiczbyParzysteNieparzyste.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'loops', path: 'loops.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'Rokprzestepny', path: 'RokPrzestepny.java', size: '1 KB', date: '2026-3-16' },
-               {name: 'Weryfikacja', path: 'Weryfikacja.java', size: '1 KB', date: '2026-3-16' },
-   
-              ],
+        java: [
+           {name: 'algorytm.java', path: 'algorytm.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'JavaCasting', path: 'JavaCasting.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'JavaConditions', path: 'JavaConditions.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'kalkulator', path: 'kalkulator.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'Liczba Pierwsza', path: 'LiczbaPierwsza.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'LiczbyParzyste', path: 'LiczbyParzysteNieparzyste.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'loops', path: 'loops.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'Rokprzestepny', path: 'RokPrzestepny.java', size: '1 KB', date: '2026-3-16' },
+            {name: 'Weryfikacja', path: 'Weryfikacja.java', size: '1 KB', date: '2026-3-16' },
+        ],
   },
 
   /* ── QUIZY ─────────────────────────────────────────────
